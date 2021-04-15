58f6b2bf839f7e36a6cb5d01c6bdeb101f2a6d7e
SooJeongSon <soboki.game@gamil.com>
2021-04-15 18:35:24 +0900

    4/15 타이머 주석 조금 추가

--- BridgeShooter/Timer.cpp
+++ BridgeShooter/Timer.cpp
@@ -4,18 +4,19 @@ HRESULT Timer::Init()
 {
 	hasGoodHardWare = false;
 	timeScale = 0.0f;		// 경과 진동수/초당 진동수 -> 한 루프가 도는 동안의 시간
-	elapsedTime = 0.0f;
-	currTime = 0.0;
-	lastTime = 0.0;
-	periodFrequency = 0.0;
+	elapsedTime = 0.0f;		// 경과 시간 : 현재시간 - 이전에 체크한 시간(last time)
+	currTime = 0.0;			// 현재 시간
+	lastTime = 0.0;			// 마지막에 체크한 시간
+	periodFrequency = 0.0;	// 
 
 	fpsTimeElapsed = 0.0f;
 	fpsFrameCount = 0;
 	
-	if (QueryPerformanceFrequency((LARGE_INTEGER*)&periodFrequency))
+	if (QueryPerformanceFrequency((LARGE_INTEGER*)&periodFrequency))	//고해상도 타이머의 주기(1초당 진동수) 반환
 	{
 		hasGoodHardWare = true;
-		QueryPerformanceCounter((LARGE_INTEGER*)&lastTime);
+		QueryPerformanceCounter((LARGE_INTEGER*)&lastTime);	
+		//수행시간동안 발생한 진동수 반환, 함수가 호출된 시점(여기선 라스트 타임)에 고해상도 타이머 값 설정
 		timeScale = 1.0f / periodFrequency;
 	}
 	else
@@ -31,7 +32,7 @@ void Timer::Tick()
 {
 	if (hasGoodHardWare)
 	{
-		QueryPerformanceCounter((LARGE_INTEGER*)&lastTime);
+		QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
 	}
 	else
 	{
