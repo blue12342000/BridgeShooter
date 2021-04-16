208849dd3b1f3efdbb40c1ada144669f0f644271
baedole1 <qodydwls1013@naver.com>
2021-04-16 17:47:03 +0900

    배용진_내 브랜치에만 올리는거 테스트

--- BridgeShooter/HomingPattern.h
+++ BridgeShooter/HomingPattern.h
@@ -2,11 +2,10 @@
 #include "Pattern.h"
 
 //가장 기본적인 미사일의 이동
-class BasicPattern : public Pattern
+class HomingPattern : public Pattern
 {
 public:
 	void Move(float deltaTime, GameObject* lpObject) override;
 
 
 };
-
