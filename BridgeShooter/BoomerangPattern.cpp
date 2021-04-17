e48b83f63e2ec7ef30a3506857813bb997f37b79
kms <kimmyoungsun24@gmail.com>
2021-04-17 21:49:23 +0900

    풀받기위한 커밋

--- BridgeShooter/BoomerangPattern.cpp
+++ BridgeShooter/BoomerangPattern.cpp
@@ -1,10 +1,16 @@
 #include "BoomerangPattern.h"
 #include "GameObject.h"
+#include"Missile.h"
 MoveInfo BoomerangPattern::Move(float deltaTime, GameObject* lpObject)
 {
     MoveInfo moveinfo = {};
     if (lpObject)
     {
+        Missile* lpMissile = (Missile*)lpObject;
+        if (typeid(*lpObject)== typeid(Missile))
+        {
+            
+        }
         if (((lpObject->elapsedTime > stopTime + stopDuration) && (lpObject->elapsedTime <= stopTime + stopDuration + 0.1f)))
         {
             lpObject->angle = atan2((WINSIZE_HEIGHT / 2) - lpObject->pos.y, (WINSIZE_WIDTH / 2) - lpObject->pos.x);
